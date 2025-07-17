cb5ec35edbe66771c973d4a6b36159cc44978f7f
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-07-17 13:15:35 +0200

    Add signup and signin pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html", [], "", "/Assets/scss/css/home.css"),
     new Route("/carpool", "Covoiturage", "/Pages/carpool.html", [], "", "/Assets/scss/css/carpool.css"),
-    new Route("/signin", "Connexion", "/Pages/authentification/signin.html", ["disconnected"], "/Assets/js/authentification/signin.js", "/Assets/scss/css/signin.css"),
-    new Route("/signup", "Inscription", "/Pages/authentification/signup.html", ["disconnected"], "/Assets/js/authentification/signup.js"),
+    new Route("/signin", "Connexion", "/Pages/authentification/signin.html", ["disconnected"], "", "/Assets/scss/css/sign.css"),///Assets/js/authentification/signin.js
+    new Route("/signup", "Inscription", "/Pages/authentification/signup.html", ["disconnected"], "", "/Assets/scss/css/sign.css"),///Assets/js/authentification/signup.js
     new Route("/account", "Mon compte", "/Pages/authentification/account.html", ["ROLE_ADMIN", "ROLE_USER"]),
     new Route("/editPSW", "Editer mot de passe", "/Pages/authentification/editPSW.html", ["ROLE_ADMIN", "ROLE_USER"]),
     new Route("/signout", "Deconnexion", "/Pages/authentification/signout.html", ["ROLE_ADMIN", "ROLE_USER"], "/Assets/js/authentification/signout.js"),
